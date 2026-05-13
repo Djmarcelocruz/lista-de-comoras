4b70693083b37a7f02866b33dfb55827b303846d
Djmarcelocruz <113671994+Djmarcelocruz@users.noreply.github.com>
2026-05-13 09:55:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'lista-familiar-v1';
 const ASSETS = [
-  './lista_familiar_pro.html',
+  './index.html',
   'https://cdn.jsdelivr.net/npm/davidshimjs-qrcodejs@0.0.2/qrcode.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
