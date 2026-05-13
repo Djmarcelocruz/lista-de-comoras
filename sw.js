426e925a90b597d783dc6a84abcb8ecb0e374a2b
Djmarcelocruz <113671994+Djmarcelocruz@users.noreply.github.com>
2026-05-13 10:25:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 const CACHE_NAME = 'lista-familiar-v1';
 const ASSETS = [
   './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdn.jsdelivr.net/npm/davidshimjs-qrcodejs@0.0.2/qrcode.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
@@ -8,15 +11,41 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return cache.addAll(ASSETS).catch((error) => {
+        console.log('Erro ao cachear alguns arquivos (esperado para CDNs):', error);
+        return cache.addAll([
+          './index.html',
+          './manifest.json',
+          './icon-192.png',
+          './icon-512.png'
+        ]);
+      });
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
     })
   );
 });
